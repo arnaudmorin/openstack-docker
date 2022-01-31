1e7c21597f0ee8bfa4727fab51bbe1b9d9f7f1f9
Arnaud Morin <arnaud.morin@ovhcloud.com>
2022-01-31 08:24:38 +0100

    Init wallaby
    Signed-off-by: Arnaud Morin <arnaud.morin@ovhcloud.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 find * -maxdepth 0 -type d | while read folder ; do
     cd $folder
-    docker build -t arnaudmorin/openstack-${folder}:victoria .
-    docker push arnaudmorin/openstack-${folder}:victoria
+    docker build -t arnaudmorin/openstack-${folder}:wallaby .
+    docker push arnaudmorin/openstack-${folder}:wallaby
     cd ..
 done
