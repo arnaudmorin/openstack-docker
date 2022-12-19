32fea4208557942d869e7a147fec03cdf596c6a0
Arnaud Morin <arnaud.morin@mailops.fr>
2022-12-19 23:21:27 +0100

    Add yoga openstack docker images
    Signed-off-by: Arnaud Morin <arnaud.morin@mailops.fr>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 find * -maxdepth 0 -type d | while read folder ; do
     cd $folder
-    docker build -t arnaudmorin/openstack-${folder}:wallaby .
-    docker push arnaudmorin/openstack-${folder}:wallaby
+    docker build -t arnaudmorin/openstack-${folder}:yoga .
+    docker push arnaudmorin/openstack-${folder}:yoga
     cd ..
 done
