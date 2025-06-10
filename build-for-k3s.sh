31475c5fbe2156d95795237f02bf18a6410f241b
Arnaud M <arnaud.choupinette@mailops.fr>
2025-06-10 20:54:20 +0200

    Add a tool to perform a local build
    Signed-off-by: Arnaud M <arnaud.choupinette@mailops.fr>

---
+++ build-for-k3s.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+find * -maxdepth 0 -type d | while read folder ; do
+    echo $folder
+done
+echo "Which the one to build and push locally? "
+read b
+pushd ${b}
+docker build . -t arnaudmorin/openstack-${b}:local
+popd
+docker save arnaudmorin/openstack-${b}:local | k3s ctr images import -
+
+echo
+echo "Image to use: arnaudmorin/openstack-${b}:local"
