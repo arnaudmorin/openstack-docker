c09fa3ece88cadc77b811435c3c353621215ecb4
Antonin Ruan <antonin.ruan@ovhcloud.com>
2026-03-25 14:08:05 -0400

    Update to 2026.1

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 find * -maxdepth 0 -type d | while read folder ; do
     cd $folder
-    sudo docker build -t arnaudmorin/openstack-${folder}:2025.2 .
-    sudo docker push arnaudmorin/openstack-${folder}:2025.2
+    docker build -t antoninruan/openstack-${folder}:2026.1 .
+    docker push antoninruan/openstack-${folder}:2026.1
     cd ..
 done
