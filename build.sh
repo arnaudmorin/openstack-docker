6cc5722fcce1fe2970c69fa20ef596e669ae2b19
Arnaud Morin <arnaud.morin@ovhcloud.com>
2026-04-13 17:26:44 +0000

    Uploading back to arnaudmorin
    Signed-off-by: Arnaud Morin <arnaud.morin@ovhcloud.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 find * -maxdepth 0 -type d | while read folder ; do
     cd $folder
-    docker build -t antoninruan/openstack-${folder}:2026.1 .
-    docker push antoninruan/openstack-${folder}:2026.1
+    docker build -t arnaudmorin/openstack-${folder}:2026.1 .
+    docker push arnaudmorin/openstack-${folder}:2026.1
     cd ..
 done
